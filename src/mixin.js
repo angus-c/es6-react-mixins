4e7cc2e2145a1d16c188f6668bbd0a3a597b14a9
Zero Cho <zcho@twitter.com>
2015-09-23 17:55:57 -0700

    support merging ES7 static props, ditch karma, use mocha + jsdom

--- src/mixin.js
+++ src/mixin.js
@@ -5,6 +5,15 @@
 
 import React from 'react';
 
+const REACT_PROPS = [
+  'childContextTypes', 'contextTypes', 'defaultProps', 'propTypes'
+];
+const REACT_LIFECYCLES = [
+  'componentWillMount', 'componentDidMount', 'componentWillReceiveProps',
+  'componentWillUpdate', 'componentDidUpdate', 'componentWillUnmount',
+  'render'
+];
+
 const noop = () => null;
 const trueNoop = () => true;
 const es6ify = (mixin) => {
@@ -18,14 +27,10 @@ const es6ify = (mixin) => {
     class NewClass extends Base {}
 
     const clonedMixin = Object.assign({}, mixin);
-    // These React properties are defined as ES7 class static properties
-    [
-      'childContextTypes', 'contextTypes',
-      'defaultProps', 'propTypes'
-    ].forEach(m => {
-      NewClass[m] = clonedMixin[m];
-      delete clonedMixin[m];
-    });
+    // move React props to NewClass's constructor
+    upgradeReactProps(NewClass, clonedMixin);
+    // merge React props defined as ES7 class static properties
+    mergeStaticProps(NewClass, clonedMixin);
     Object.assign(NewClass.prototype, clonedMixin);
 
     return NewClass;
@@ -38,10 +43,7 @@ const mixin = (...mixins) => {
   Base.prototype.shouldComponentUpdate = trueNoop;
 
   // No-ops so we need not check before calling super()
-  ['componentWillMount', 'componentDidMount', 'componentWillReceiveProps',
-   'componentWillUpdate', 'componentDidUpdate', 'componentWillUnmount',
-   'render'].
-    forEach(m => Base.prototype[m] = noop);
+  REACT_LIFECYCLES.forEach(m => Base.prototype[m] = noop);
 
   mixins.reverse();
 
@@ -49,4 +51,28 @@ const mixin = (...mixins) => {
   return Base;
 };
 
+function upgradeReactProps(klass, mixin) {
+  const staticProps = REACT_PROPS.reduce((result, prop) => {
+    klass[prop] = mixin[prop];
+    delete mixin[prop];
+    return result;
+  }, {});
+  return klass;
+}
+
+function mergeStaticProps(klass, clonedMixin) {
+  const superKlass = Object.getPrototypeOf(klass);
+  const mixin = clonedMixin || superKlass;
+  Object.keys(mixin).forEach(m => {
+    if (typeof m !== 'function') {
+      klass[m] = Object.assign(klass[m] || {}, mixin[m]);
+    }
+  });
+
+  return klass;
+}
+
+export {
+  mergeStaticProps
+};
 export default mixin;
